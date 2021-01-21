2883cb2167b3cdc1f625ffb61400ccbd3011dcfa
chirsmarti343 <christianjosue321@gmail.com>
2021-01-21 14:08:34 -0700

    lesson3

---
+++ lesson3/main.js
@@ -0,0 +1,9 @@
+// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();  
+
+// Last time web page updated
+
+var x = new Date(document.lastModified);
+
+document.getElementById("update").innerHTML = x;    
