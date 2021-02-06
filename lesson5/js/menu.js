342cf90ecaf41cfe354809f90d3d30ecfc69f48e
chirsmarti343 <christianjosue321@gmail.com>
2021-02-05 20:08:52 -0700

    new_one

---
+++ lesson5/js/menu.js
@@ -0,0 +1,31 @@
+
+
+window.addEventListener('load',()=>{
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click',()=> {mainnav.classList.toggle('responsive')},false);
+});
+
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+var weekday=new Array(7);
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
+
+var n =  new Date();
+var y = n.getFullYear();
+var m = n.getMonth();
+var d = n.getDate();
+var day = n.getDay();
+console.log(weekday[day])
+document.getElementById("date").innerHTML = weekday[day] + ", "  + d + " "+ monthNames[m] +" " + y ;
+
+                  
