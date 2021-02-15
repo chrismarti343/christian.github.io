8d903900a1d9ff09f297abcfd939d109ca752709
chirsmarti343 <christianjosue321@gmail.com>
2021-02-14 22:14:27 -0700

    changs

--- lesson6/js/menu.js
+++ lesson6/js/menu.js
@@ -43,5 +43,15 @@ function showhide(){
 showhide();
 
 
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:6,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
+  myMap();
+
+
 
                   
