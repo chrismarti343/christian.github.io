b3dbb8da2dfae19ea22e127fa26b87e9da1df383
chirsmarti343 <christianjosue321@gmail.com>
2021-02-17 23:19:05 -0700

    eliminate_somethigns

--- lesson6/js/menu.js
+++ lesson6/js/menu.js
@@ -33,7 +33,7 @@ function showhide(){
   var day = n.getDate();
   console.log(day);
 
-  if (day == 3){
+  if (day == 5){
     var show = document.getElementById("ad_conteiner");
     console.log(show);
     show.style.display = (show.style.display == 'block') ? 'none' : 'block';
