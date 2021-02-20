0baafa188f0e836b4c2270cf2fbf7bb541b89d7c
chirsmarti343 <christianjosue321@gmail.com>
2021-02-20 06:49:55 -0700

    many_changes

---
+++ lesson7/js/font.js
@@ -0,0 +1,12 @@
+
+
+WebFont.load({
+    google: {
+      families: ['Pangolin', 'Concert One','Rubik','Potta One']
+    },
+    active: function(){ 
+      //when fonts loaded
+      console.log('fonts loaded');
+  }
+  });
+
