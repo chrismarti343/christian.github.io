2ba740e9964bfdb1c5aef28a9e5be3443d1a658d
chirsmarti343 <christianjosue321@gmail.com>
2021-03-05 10:44:23 -0700

    little_changes_ad

---
+++ lesson9/js/menu.js
@@ -0,0 +1,47 @@
+
+
+window.addEventListener('load',()=>{
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click',()=> {mainnav.classList.toggle('responsive')},false);
+});
+
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+var weekday=new Array(7);
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
+
+var n =  new Date();
+var y = n.getFullYear();
+var m = n.getMonth();
+var d = n.getDate();
+var day = n.getDay();
+console.log(weekday[day])
+document.getElementById("date").innerHTML = weekday[day] + ", "  + d + " "+ monthNames[m] +" " + y ;
+
+function showhide(){
+  var n =  new Date();
+  var day = n.getDate();
+  console.log(day);
+
+  if (day == 5){
+    var show = document.getElementById("ad_conteiner");
+    console.log(show);
+    show.style.display = (show.style.display == 'block') ? 'none' : 'block';
+  } 
+  
+}
+showhide();
+
+
+
+                  
