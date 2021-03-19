a4b320d7031fd75564751a2e6c2f29ac0b4d0dff
chirsmarti343 <christianjosue321@gmail.com>
2021-03-18 20:05:37 -0600

    changes

---
+++ web_site/js/fish-weather.js
@@ -0,0 +1,16 @@
+
+
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=f1e877f0bd9a6d243ab40b2ed13c9fcd&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('description').textContent = jsObject.weather[0].description;
+    document.getElementById('temperature').textContent = jsObject.main.temp;
+    document.getElementById('temp_max').textContent = jsObject.main.temp_max;
+    document.getElementById('temp_min').textContent = jsObject.main.temp_min;
+    document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+
+  });
