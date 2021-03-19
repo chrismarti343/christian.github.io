38e2d420c496aee5fe989dc134de6fa9e19fde93
chirsmarti343 <christianjosue321@gmail.com>
2021-03-18 23:23:22 -0600

    mrore

--- web_site/js/event.js
+++ web_site/js/event.js
@@ -1,3 +1,6 @@
+
+
+
 function getEvents(cityName){
 
     fetch('https://byui-cit230.github.io/weather/data/towndata.json')
@@ -5,23 +8,24 @@ function getEvents(cityName){
       return result.json();
     })
     .then(
-       (result) => {
+      (result) => {
           result.towns.forEach(
             (town) => {
-             if (town.name.toLowerCase() === cityName.toLowerCase()) {
-                 town.events.forEach(
+            if (town.name.toLowerCase() === cityName.toLowerCase()) {
+                town.events.forEach(
                   event => {
                     let li = document.createElement('li');
                     li.textContent = event;
                     console.log(town.events);
                     document.querySelector('#events').appendChild(li);
                   }
-                 )
-               }
+                )
+              }
               }
             )
-       }
+      }
     ); 
     }
+ 
 
 getEvents(cityName);
